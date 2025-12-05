c3813da1a2ad916659e70207640a079cd82d23ff
hyerin9031 <148173524+hyerin9031@users.noreply.github.com>
2025-12-06 04:33:31 +0900

    Update PlayerMain.java

--- PlayerMain.java
+++ PlayerMain.java
@@ -1,10 +1,19 @@
 package termproject;
 
+import java.net.*;
+
 public class PlayerMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Socket playersocket = null;
+		try {
+			playersocket = new Socket("localhost", 7400);
+			new PlayerHomeFrame(playersocket);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
-
+	
 }
+
